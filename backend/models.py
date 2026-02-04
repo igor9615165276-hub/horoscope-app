793b9ea73c847039565084cea92f3166309cea90
igor9615165276-hub <igor9615165276@gmail.com>
2026-02-04 20:16:01 +0300

    Fix models indentation

--- backend/models.py
+++ backend/models.py
@@ -11,7 +11,7 @@ from sqlalchemy import (
     func,
 )
 from sqlalchemy.dialects.postgresql import UUID
-    from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship
 
 from .database import Base
 
